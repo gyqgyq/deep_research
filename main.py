d3f70316d467fd7a6bb234d2de9e357566dd5a0d
gyqgyq <510743998@qq.com>
2026-07-25 00:24:01 +0800

    feat: add fastapi

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-def main():
-    print("Hello from backend!")
+from fastapi import FastAPI
 
+app = FastAPI(title="UV + FastAPI Demo")
+
+@app.get("/")
+async def root():
+    return {"msg": "Hello FastAPI with uv"}
 
 if __name__ == "__main__":
-    main()
+    import uvicorn
+    uvicorn.run(
+        "main:app",
+        host="0.0.0.0",
+        port=8000,
+        reload=True,
+    )
